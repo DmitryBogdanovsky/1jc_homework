8aff3e8e6afc701bac6cddb96b7b1489b35353b2
Dmitry Bogdanovsky <d.a.bogdanovsky@gmail.com>
2022-04-05 14:44:21 +0300

    Task28

--- src/Homework4/Task28.java
+++ src/Homework4/Task28.java
@@ -1,4 +1,20 @@
 package Homework4;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Task28 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println(" Input text : " );
+        String line = in.nextLine();
+
+        Pattern pattern = Pattern.compile("([a-zа-я])\\b");
+        Matcher matcher = pattern.matcher(line);
+        while (matcher.find()) {
+            System.out.print(matcher.group(1));
+        }
+    }
 }
