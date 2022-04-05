7a4c63bea5310a123a76d0de592dd27b738d4fe6
Dmitry Bogdanovsky <d.a.bogdanovsky@gmail.com>
2022-04-05 14:44:44 +0300

    Task29

--- src/Homework4/Task29.java
+++ src/Homework4/Task29.java
@@ -1,4 +1,24 @@
 package Homework4;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Task29 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println(" Input string : ");
+        String line = in.nextLine();
+        Pattern pattern = Pattern.compile("0x[A-Fa-f0-9]{1,4}");
+        Matcher matcher = pattern.matcher(line);
+        System.out.println("Places where hexadecimal numbers are found: ");
+        while (matcher.find()) {
+            System.out.println(matcher.start() + " " + matcher.end());
+        
+        }
+
+    }
+
+
 }
