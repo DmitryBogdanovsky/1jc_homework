9fdf2ef40fae3fd868a86ac135af306297ee3a5b
Dmitry Bogdanovsky <d.a.bogdanovsky@gmail.com>
2022-04-05 14:39:06 +0300

    Task30

---
+++ src/Homework4/Task30.java
@@ -0,0 +1,16 @@
+package Homework4;
+
+
+import java.util.Scanner;
+
+public class Task30 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.println(" Input string : ");
+        String line = in.nextLine();
+
+        System.out.println(line.replaceAll("<p id=p1>", "<p>"));
+    }
+
+}
