373b7fbbf80def5ea5098a151049ba3208830ffa
Dmitry Bogdanovsky <d.a.bogdanovsky@gmail.com>
2022-04-05 15:02:54 +0300

    Task30

--- src/Homework4/Task30.java
+++ src/Homework4/Task30.java
@@ -6,9 +6,8 @@ import java.util.Scanner;
 public class Task30 {
     public static void main(String[] args) {
 
-        Scanner in = new Scanner(System.in);
-        System.out.println(" Input string : ");
-        String line = in.nextLine();
+        StringInput stringInput = new StringInput();
+        String line = stringInput.input();
         System.out.println(line.replaceAll("<p id=p1>", "<p>"));
     }
 
