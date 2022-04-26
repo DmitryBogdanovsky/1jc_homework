c97f53f3e781b82943836aff389cc8ed85fb21b7
Dmitry Bogdanovsky <d.a.bogdanovsky@gmail.com>
2022-04-26 06:40:09 +0300

    Task31

--- src/Homework4/Task31.java
+++ src/Homework4/Task31.java
@@ -5,31 +5,31 @@ public class Task31 {
         long time1 = strConcat();
         long time2 = buldConcat();
         if (time1 > time2)
-            System.out.println("Builder fast then String on "+ (time1 - time2));
+            System.out.println("Builder fast then String on " + (time1 - time2));
         else
-            System.out.println("String fast then Builder on "+ (time2 - time1));
+            System.out.println("String fast then Builder on " + (time2 - time1));
 
     }
 
     public static long strConcat() {
         long start = System.currentTimeMillis();
         String s1 = "";
-        for (int i = 0; i < 1_000_000; i++) {
+        for (int i = 0; i < 100_000; i++) {
             s1 = s1 + "aaabbbccc";
         }
-        System.out.println(System.currentTimeMillis() - start);
         long start1 = System.currentTimeMillis() - start;
+        System.out.println(start1);
         return start1;
     }
 
     public static long buldConcat() {
         long start2 = System.currentTimeMillis();
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < 1_000_000; i++) {
+        for (int i = 0; i < 100_000; i++) {
             sb.append("aaabbbccc");
         }
-        System.out.println(System.currentTimeMillis() - start2);
         long start3 = System.currentTimeMillis() - start2;
+        System.out.println(start3);
         return start3;
     }
 }
