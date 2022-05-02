2ee4827be2ed0fdd29ed042843f192b68311a6fd
Dmitry Bogdanovsky <d.a.bogdanovsky@gmail.com>
2022-05-02 11:31:51 +0300

    Task25

--- src/Homework2/Task17.java
+++ src/Homework2/Task17.java
@@ -2,7 +2,7 @@ package Homework2;
 
 public class Task17 {
     public static void main(String[] args) {
-        System.out.printf("%,d", 200023143); // 200023143 --> "200 023 143"
+        System.out.printf("%,d", 2023143); // 200023143 --> "200 023 143"
 
     }
 
