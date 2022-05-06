59d8778e8239eb540ae2ec50046d885465e60013
Dmitry Bogdanovsky <d.a.bogdanovsky@gmail.com>
2022-05-06 17:30:22 +0300

    Task42

---
+++ src/Homework6/Task42/Main.java
@@ -0,0 +1,8 @@
+package Homework6.Task42;
+
+public class Main {
+    public static void main(String[] args) {
+        MapCollection words = new MapCollection();
+        words.countWords();
+    }
+}
